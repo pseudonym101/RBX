abfa94dd3c990f83324a80bf1316779debe7e774
pseudonym101 <pudvoi7@gmail.com>
2025-02-21 22:36:52 +0700

    Update list1.lua

--- list1.lua
+++ list1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/pseudonym101/RBX/refs/heads/path1/1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pseudonym101/RBX/refs/heads/path1/2.lua"))()
