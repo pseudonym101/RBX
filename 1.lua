a699487f5fef72d7cdacf38609367fbc2e9eaf5f
pseudonym101 <pudvoi7@gmail.com>
2025-02-22 14:19:59 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -226,7 +226,11 @@ end
 
 --redz
 local function zredz()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+	local Settings = {
+  		JoinTeam = "Marines";
+  		Translator = false;
+	}
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
 end
 
 
