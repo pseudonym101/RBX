a973119750f99ea35f063517cab58c5bb85a343a
pseudonym101 <pudvoi7@gmail.com>
2025-08-05 20:16:18 +0700

    Update Gate.lua

--- Gate.lua
+++ Gate.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/pseudonym101/RBX/main/list.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/pseudonym101/RBX/path1/list.lua"))()
 
 local URL = Games[game.PlaceId]
 
