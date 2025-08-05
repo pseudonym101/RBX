ea4d60c4f52d784243623f99c5fb6352a5e37351
pseudonym101 <pudvoi7@gmail.com>
2025-08-05 20:38:28 +0700

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,9 @@
+local Games = {
+	[2753915549] = "", -- Blox Fruit Sea 1
+	[4442272183] = "", -- Blox Fruit Sea 2
+	[7449423635] = "", -- Blox Fruit Sea 3
+	[16775404767] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/customCarTycoon.lua", -- Custom Car Tycoon
+
+}
+
+return Games
