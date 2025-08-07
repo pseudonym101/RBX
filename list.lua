bdefa5224bdf8371b64863ac4cc3e9883b2b227d
pseudonym101 <pudvoi7@gmail.com>
2025-08-07 15:52:16 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -3,6 +3,7 @@ local Games = {
 	[4442272183] = "", -- Blox Fruit Sea 2
 	[7449423635] = "", -- Blox Fruit Sea 3
 	[16775404767] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/customCarTycoon.lua", -- Custom Car Tycoon
+	[6442957604] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/customPCTycoon.lua", -- Custom PC Tycoon
 
 }
 
