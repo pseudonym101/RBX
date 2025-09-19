59fff4a67bb572215c0be50d1257a7decf0d8c24
pseudonym101 <pudvoi7@gmail.com>
2025-09-19 15:32:45 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -8,7 +8,9 @@ local Games = {
 	[135630848872701] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/kingBoxing.lua", -- King Boxing
 	[102686217723755] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/rebornCultivation.lua", -- Reborn Cultivation
 	[72712036210947] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/fixItUp.lua", -- Fix It Up
-
+	[16041084629] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/animeSpirit.lua", --anieme spirit sea1
+	[11756036029] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/animeSpirit.lua", --anieme spirit sea2
+	[73417524077325] = "https://raw.githubusercontent.com/pseudonym101/RBX/path1/animeSpirit.lua", --anieme spirit space
 }
 
 return Games
